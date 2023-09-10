097c1f12c918ed6b06f889a71d75bf94bb155b77
Faid Terence JABO <faidterence@outlook.com>
2023-09-10 13:41:35 +0200

    File Upload to firebase

--- src/pages/api/firestore.ts
+++ src/pages/api/firestore.ts
@@ -1,8 +1,7 @@
-import { storage, app, database } from "@/firebaseConfig";
 import { type NextApiRequest, type NextApiResponse } from "next";
 
 const firestore = (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json({text: "Hello World"});
-}
+  res.status(200).json({ text: "Welcome to Terence Faid JABO" });
+};
 
 export default firestore;
